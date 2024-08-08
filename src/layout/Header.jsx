7c691f14deec852d8749f409aab913094706efe2
1RahulP <007rahulpundir@gmail.com>
2024-08-09 00:10:54 +0530

    menu

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,6 +1,8 @@
+import React from "react"
 const Header = () =>{
+    const [showMenu,setShowMenu] = React.useState()
     return(
-        <div className="flex justify-between sm:px-16 px-4 py-4 shadow-sm">
+        <div className="flex justify-between sm:px-16 px-4 py-4 shadow-sm relative">
             <div>
                 <span className="text-xl font-semibold">Andaman.In</span>
             </div>
@@ -10,8 +12,17 @@ const Header = () =>{
                     <li className="cursor-pointer">Shop</li>
                     <li className="cursor-pointer">The Andamans Wiki</li>
                 </ul>
-                <div>
-                    <img src="/svg/menu.svg" alt="menu" className="w-[32px]" />
+                <div className="sm:hidden block" >
+                    <img src="/svg/menu.svg" alt="menu" className="w-[32px]" onClick={()=>setShowMenu(!showMenu)} />
+                    {showMenu &&
+                    <div className="backdrop-blur-xl z-[99] absolute right-0 w-full h-screen">
+                        <div className="w-[200px] h-screen bg-white border absolute right-0 z-[9] p-2">
+                            <div className="mb-2 cursor-pointer">About</div>
+                            <div className="mb-2 cursor-pointer">Shop</div>
+                            <div className="cursor-pointer">The Andamans Wiki</div>
+                        </div>
+                        </div>
+                    }
                 </div>
             </div>
         </div>
